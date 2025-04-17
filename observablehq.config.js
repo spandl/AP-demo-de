69f5d2c0764c7ece82f823543e3589c8fdcc3f91
Ralph Spandl <ralph@r42.ca>
2025-04-17 14:10:00 +0200

    Favicon

--- observablehq.config.js
+++ observablehq.config.js
@@ -22,7 +22,7 @@ export default {
   ],
 
   // Content to add to the head of the page, e.g. for a favicon:
-  head: '<link rel="icon" href="observable.png" type="image/png" sizes="32x32">',
+  head: '<link rel="icon" href="https://r42.ca/favicon/favicon.svg" type="image/png" sizes="32x32">',
 
   // The path to the source root.
   root: "src",
